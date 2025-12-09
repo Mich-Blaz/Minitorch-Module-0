43fec1025c286de1de473d172905e58d5dfd1386
Michel Blazevic <michel.blazevic@safrangroup.com>
2025-12-09 14:32:22 +0100

    complete operators from task 0-1

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -5,6 +5,66 @@ import math
 # ## Task 0.1
 from typing import Callable, Iterable
 
+
+def mul(x : float,y : float) -> float:
+    return x*y
+
+def add(x : float,y : float) -> float:
+    return x+y
+
+def id(i):
+    return i
+
+def neg(x : float) -> float:
+    return -1 * x
+
+def lt(x : float , y : float) -> bool:
+    return x < y 
+
+def eq(x : float, y: float) -> bool:
+    return x == y
+
+def max(x : float , y : float) -> float:
+    return max(x,y)
+
+def is_close(x: float , y : float , threshold : float =1e-2) -> bool : 
+    return abs(x-y) < threshold
+
+def exp(x : float) -> float : 
+    return math.exp(x)
+
+def sigmoid(x : float) -> float:
+    return  1 / (1 + exp(-x))
+    # if x >=0 :
+    #     return  1 / (1 + exp(-x))
+    # else:
+    #     return exp(x) / (1 + exp(x))
+
+
+def inv(x : float) -> float : 
+    try : 
+        r = 1 / x 
+        return r 
+    except ZeroDivisionError:
+        return 1e16
+    
+def log(x : float) -> float : 
+    return math.log(x)
+
+
+def log_back(x : float, y : float) -> float : 
+    return y * (1 / x)
+    
+def inv_back(x : float,y:float) -> float : 
+    return y * (-1 / x**2)
+
+def relu_back(x : float , y : float) -> float: 
+    return y if x > 0 else 0
+
+
+def relu(x : float) -> float: 
+    return x if x>0 else 0
+
 #
 # Implementation of a prelude of elementary functions.
 
